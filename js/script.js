95eb6eabb8034a7a17d21e41201ed0ad091c7eb6
Kilhog <kilhog@protonmail.com>
2015-04-30 22:37:43 +0200

    Passage a Material In Progress

--- js/script.js
+++ js/script.js
@@ -1,18 +1,61 @@
-(function () {
-  'use strict';
-  angular.module('tabsDemo1', ['ngMaterial'] ).controller('AppCtrl', AppCtrl);
-  function AppCtrl ( $scope ) {
-    $scope.data = {
-      selectedIndex: 0,
-      secondLocked:  true,
-      secondLabel:   "Item Two",
-      bottom:        false
-    };
-    $scope.next = function() {
-      $scope.data.selectedIndex = Math.min($scope.data.selectedIndex + 1, 2) ;
-    };
-    $scope.previous = function() {
-      $scope.data.selectedIndex = Math.max($scope.data.selectedIndex - 1, 0);
-    };
-  }
-})();
+'use strict';
+
+var apiBetaseries = require("./dist/js/api-betaseries");
+var apiGetStrike = require("./dist/js/api-getstrike");
+var apiTransmission = require("./dist/js/api-transmission");
+var apiDblite = require("./dist/js/api-dblite");
+var apiAddicted = require("./dist/js/api-addicted");
+
+var ipc = require('ipc');
+var justOpen = true;
+var buttonCloseIsBind = false;
+
+var app = angular.module('adm-app', ["ngMaterial", "ui.router"]);
+
+app.config(["$stateProvider", "$urlRouterProvider", "$mdThemingProvider",
+  function ($stateProvider, $urlRouterProvider, $mdThemingProvider) {
+    $mdThemingProvider.theme('default').primaryPalette('grey',{
+      'default': '800'
+    }).accentPalette('indigo',{
+      'default': '200'
+    });
+
+    $urlRouterProvider.otherwise('/main/manager');
+
+    $stateProvider
+      // States
+      .state("main", {
+        controller: 'mainCtrl',
+        url: "/main",
+        templateUrl: 'partial/main.html'
+      })
+      .state("main.reglages", {
+        controller: 'mainCtrl',
+        parent: 'main',
+        url: "/reglages",
+        templateUrl: 'partial/reglages.html'
+      })
+      .state("main.manager", {
+        controller: 'mainCtrl',
+        parent: 'main',
+        url: "/manager",
+        templateUrl: 'partial/manager.html'
+      });
+    }
+]).filter('numberFixedLen', function () {
+  return function (n, len) {
+    var num = parseInt(n, 10);
+    len = parseInt(len, 10);
+    if (isNaN(num) || isNaN(len)) {
+      return n;
+    }
+    num = '' + num;
+    while (num.length < len) {
+      num = '0' + num;
+    }
+    return num;
+  };
+}).controller('mainCtrl', ["$scope", "$timeout", "$filter",
+  function ($scope, $timeout, $filter, Notification, $modal) {
+
+  }]);
