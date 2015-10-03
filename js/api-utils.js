17d2ae966211c23c6b43fce74be19ff9094cf24e
Kilhog <kilhog@protonmail.com>
2015-10-03 15:55:54 +0200

    Encrypt DB

--- js/api-utils.js
+++ js/api-utils.js
@@ -1,13 +1,32 @@
 (function() {
 
+  var CryptoJS = require("crypto-js");
+  var key = "Th3M0stUs3l33sK3y1nTh3W0rld";
 
   function utils() {
 
   }
 
+  /**
+   * Encrypte les params
+   * @param param
+   */
+  function encrypt_param(param) {
+    return CryptoJS.AES.encrypt(param, key);
+  }
+
+  /**
+   * Décrypte les params
+   * @param param
+   */
+  function decrypt_param(param) {
+    return CryptoJS.AES.decrypt(param, key);
+  }
+
   utils.getParam = function(db, param, func, is_json) {
     db.query("SELECT * FROM params WHERE nom = ?", [param], function(err, rows) {
       if (rows.length > 0) {
+        rows[0][2] = decrypt_param(rows[0][2]).toString(CryptoJS.enc.Utf8);
         func(is_json ? JSON.parse(rows[0][2]) : rows[0][2]);
       } else {
         func(is_json ? {} : undefined);
@@ -16,8 +35,9 @@
   };
 
   utils.setParam = function(db, param, data, func, is_json) {
+    data = encrypt_param(is_json ? JSON.stringify(data) : data).toString();
     db.query("DELETE FROM params WHERE nom = ?", [param], function() {
-      db.query("INSERT INTO params (nom, value) VALUES (?, ?)", [param, is_json ? JSON.stringify(data) : data], function() {
+      db.query("INSERT INTO params (nom, value) VALUES (?, ?)", [param, data], function() {
         func();
       });
     });
