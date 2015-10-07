cdd4301efdb7a4058f72aefc0dd437cb3fa2de9a
Kilhog <kilhog@protonmail.com>
2015-10-07 15:15:29 +0200

    Modification path db + Update package.json

--- js/api-dblite.js
+++ js/api-dblite.js
@@ -1,7 +1,9 @@
 (function() {
   var path = require('path-extra');
-
   var dblite = require('dblite');
+
+  var pathdb = path.datadir('adm');
+
   if(process.platform == "win32") {
     dblite.bin = __dirname + "/../../lib/sqlite3/win32/sqlite3.exe";
   } else if(process.platform == "darwin") {
@@ -13,12 +15,14 @@
   var fs = require('fs');
 
   try {
-    fs.mkdirSync(path.homedir() + '/.adm');
+    fs.mkdirSync(pathdb);
   } catch(e) {
     // Already Exist
   }
   
-  var path_db = path.homedir() + '/.adm/adm.sqlite';
+  var path_db = pathdb + '/adm.sqlite';
+
+console.log(path_db);
 
   var apiDblite = function() {
     this.db = dblite(path_db);
@@ -29,7 +33,6 @@
 
   apiDblite.prototype.createDb = function(func) {
     var self = this;
-
   };
 
   apiDblite.dropDb = function(func) {
