314d59cec78444ac610983e2baceaba78cd7d50c
Kilhog <kilhog@protonmail.com>
2015-10-24 14:45:43 +0200

    Fix bug Minify JS

--- js/episode_bottom_sheet.js
+++ js/episode_bottom_sheet.js
@@ -1,15 +1,15 @@
-app.controller('ListBottomSheetCtrl', function($scope, $mdBottomSheet, episode) {
+app.controller('ListBottomSheetCtrl', ['$scope', '$mdBottomSheet', 'episode',
+  function($scope, $mdBottomSheet, episode) {
+    $scope.episode = episode;
 
-  $scope.episode = episode;
-
-  $scope.seenEpisode = function (){
-    $mdBottomSheet.hide("seen");
-  };
-  $scope.downloadEpisode = function (){
-    $mdBottomSheet.hide("download");
-  };
-  $scope.downloadStr = function (){
-    $mdBottomSheet.hide("str");
-  };
-
-});
+    $scope.seenEpisode = function (){
+      $mdBottomSheet.hide("seen");
+    };
+    $scope.downloadEpisode = function (){
+      $mdBottomSheet.hide("download");
+    };
+    $scope.downloadStr = function (){
+      $mdBottomSheet.hide("str");
+    };
+  }
+]);
