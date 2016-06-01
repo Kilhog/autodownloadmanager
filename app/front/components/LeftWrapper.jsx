5e982b848fa593e120eac727820144b28ff661ba
Kilhog <kilhog@protonmail.com>
2016-06-01 22:59:42 +0200

    Style - In Progress

--- app/front/components/LeftWrapper.jsx
+++ app/front/components/LeftWrapper.jsx
@@ -4,19 +4,21 @@ import mui from 'material-ui';
 class LeftWrapper extends Component {
   render() {
     return (
-      <div className="guilds-wrapper">
-        <div className="guilds">
-          <div className="guild">
-            <div className="guild-inner" draggable="false" style={{borderRadius: '25px', backgroundColor: 'rgb(46, 49, 54)'}}><a draggable="false" className="" href="/channels/@me">
-              <div className="friends-icon"></div>
-            </a></div>
+      <div className="left-wrapper">
+        <div className="icones-items">
+          <div className="icone-item">
+            <div className="icone-item-inner" style={{borderRadius: '25px', backgroundColor: 'rgb(46, 49, 54)'}}>
+              <a>
+                <div className="friends-icon"></div>
+              </a>
+            </div>
           </div>
           <div className="friends-online">0 en ligne</div>
           <span className="dms"/>
-          <div className="guild-separator"></div>
-          <div className="guild selected unread">
+          <div className="icone-item-separator"></div>
+          <div className="icone-item selected unread">
             <div draggable="true">
-              <div className="guild-inner" draggable="false" style={{borderRadius: '15px'}}>
+              <div className="icone-item-inner" draggable="false" style={{borderRadius: '15px'}}>
                 <a draggable="false" className="avatar-small" href="https://discordapp.com/assets/channels/110838748563914752/163668753874288640" style={{backgroundImage: 'url("https://cdn.discordapp.com/icons/110838748563914752/5d7eab04da5e6c5fb191cc3a016a685c.jpg")'}}/>
               </div>
             </div>
